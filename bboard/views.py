7b27d3c85011f44cf54ef1b5f2613c5a8dac809d
Abilmansur <mansur.sadyrbek09@gmail.com>
2026-03-31 19:28:01 +0500

    Added pagination to tasks

--- bboard/views.py
+++ bboard/views.py
@@ -1,15 +1,19 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Task
-
 
 def index(request):
     return render(request, 'index.html')
 
-
 def tasks_page(request):
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {'tasks': tasks})
 
+    paginator = Paginator(tasks, 2)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'tasks.html', {'page_obj': page_obj})
 
 def about(request):
-    return render(request, 'about.html', {'text': "Очень длинный текст для примера"})
+    return render(request, 'about.html')
